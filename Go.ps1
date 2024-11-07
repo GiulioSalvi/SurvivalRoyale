877cfa6db21f3c972550bb438a395dd00a474c88
Giulio Salvi <salvigiulio@icloud.com>
2024-11-07 11:42:14 +0100

    Updated 'Go.ps1'

--- Go.ps1
+++ Go.ps1
@@ -1,6 +1,6 @@
 Clear-Host
 
 gcc -w -c src/ansi.c -o bin/objects/ansi-macos-c99.o -std=c99
-gcc -w src/main.c bin/objects/ansi-macos-c99.o bin/objects/type-macos-c99.o -o bin/exec/project -std=c99 -lncurses
+gcc -w src/main.c bin/objects/ansi-macos-c99.o -o bin/exec/project -std=c99
 
 ./bin/exec/project
