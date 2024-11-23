bd609078e5fa6bc51ecb008cfcf54a556fcac9f5
Giulio Salvi <salvigiulio@icloud.com>
2024-11-23 12:49:39 +0100

    Moved ANSI enums in to the 'ansi_const.h' header and saved as const uint8_t.

---
+++ h/ansi_const.h
@@ -0,0 +1,55 @@
+#include "includes.h"
+
+/// @brief ANSI standard black foreground color
+const uint8_t FgBlack = 30;
+/// @brief ANSI standard red foreground color
+const uint8_t FgRed = 31;
+/// @brief ANSI standard green foreground color
+const uint8_t FgGreen = 32;
+/// @brief ANSI standard yellow foreground color
+const uint8_t FgYellow = 33;
+/// @brief ANSI standard blue foreground color
+const uint8_t FgBlue = 34;
+/// @brief ANSI standard magenta foreground color
+const uint8_t FgMagenta = 35;
+/// @brief ANSI standard cyan foreground color
+const uint8_t FgCyan = 36;
+/// @brief ANSI standard white foreground color
+const uint8_t FgWhite = 37;
+/// @brief ANSI standard bright black foreground color
+const uint8_t FgBrightBlack = 90;
+/// @brief ANSI standard bright red foreground color
+const uint8_t FgBrightRed = 91;
+/// @brief ANSI standard bright green foreground color
+const uint8_t FgBrightGreen = 92;
+/// @brief ANSI standard bright yellow foreground color
+const uint8_t FgBrightYellow = 93;
+/// @brief ANSI standard bright blue foreground color
+const uint8_t FgBrightBlue = 94;
+/// @brief ANSI standard bright magenta foreground color
+const uint8_t FgBrightMagenta = 95;
+/// @brief ANSI standard bright cyan foreground color
+const uint8_t FgBrightCyan = 96;
+/// @brief ANSI standard bright white foreground color
+const uint8_t FgBrightWhite = 97;
+
+const uint8_t BgBlack = 40;
+const uint8_t BgRed = 41;
+const uint8_t BgGreen = 42;
+const uint8_t BgYellow = 43;
+const uint8_t BgBlue = 44;
+const uint8_t BgMagenta = 45;
+const uint8_t BgCyan = 46;
+const uint8_t BgWhite = 47;
+const uint8_t BgBrightBlack = 100;
+const uint8_t BgBrightRed = 101;
+const uint8_t BgBrightGreen = 102;
+const uint8_t BgBrightYellow = 103;
+const uint8_t BgBrightBlue = 104;
+const uint8_t BgBrightMagenta = 105;
+const uint8_t BgBrightCyan = 106;
+const uint8_t BgBrightWhite = 107;
+
+const uint8_t No = 20;
+const uint8_t SinglyUnderlined = 21;
+const uint8_t DoublyUnderlined = 22;
