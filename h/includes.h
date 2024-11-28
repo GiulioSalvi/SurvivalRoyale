2f3c9fe031cbcc76aba0a25d2f96f55f262b5eb8
Giulio Salvi <salvigiulio@icloud.com>
2024-11-28 09:24:57 +0100

    Various code edits.
    Removed the #ifndef precompiler directives and addded the header library 'sys/ioctl.h' for Unix-based and POSIX-compliant platforms.

--- h/includes.h
+++ h/includes.h
@@ -1,27 +1,22 @@
-#ifndef INCLUDES
-    #define INCLUDES
+#define NONE 1
 
-    #ifndef NONE
-        #define NONE -1
-    #endif
-
-    #ifdef _WIN32
-        #include <conio.h>
-        #include <windows.h>
-    #else
-        #include <unistd.h>
-        #include <termios.h>
-    #endif
-
-    #include <math.h>
-    #include <time.h>
-    #include <errno.h>
-    #include <stdio.h>
-    #include <assert.h>    
-    #include <stdarg.h>
-    #include <stdint.h>
-    #include <stdlib.h>
-    #include <string.h>
-    #include <stdbool.h>
-    #include <stdalign.h>
+#ifdef _WIN32
+    #include <conio.h>
+    #include <windows.h>
+#else
+    #include <unistd.h>
+    #include <termios.h>
+    #include <sys/ioctl.h>
 #endif
+
+#include <math.h>
+#include <time.h>
+#include <errno.h>
+#include <stdio.h>
+#include <assert.h>    
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdalign.h>
